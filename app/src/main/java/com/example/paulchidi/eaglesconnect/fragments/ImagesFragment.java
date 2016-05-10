f9d0848394648a6bb92f665a0fed64c44979b56f
paul6ix <okporp@gmail.com>
2016-05-10 04:49:47 +0100

    working on profile

--- app/src/main/java/com/example/paulchidi/eaglesconnect/fragments/ImagesFragment.java
+++ app/src/main/java/com/example/paulchidi/eaglesconnect/fragments/ImagesFragment.java
@@ -56,7 +56,7 @@ public class ImagesFragment extends Fragment {
                 return imageView;
             }
         });
-        
+
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
